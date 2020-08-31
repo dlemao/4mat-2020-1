bcbadba873908b6b2cae2ec80ca3458ad58a9e63
dlemao <hilgemanndaniel@gmail.com>
2020-08-31 12:39:16 +0000

    (26/08) Instalação do mongoose e configuração para usar o Mongodb.
    Signed-off-by: dlemao <hilgemanndaniel@gmail.com>

--- back-end/app.js
+++ back-end/app.js
@@ -4,15 +4,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users'); 
+var usersRouter = require('./routes/users');
+
+const db = require('./Config/database')
+db('mongodb+srv://daniel:Marina1308@cluster0.4axde.gcp.mongodb.net/?retryWrites=true&w=majority')
 
 var app = express();
-
-const cors = require('cors')
-app.use(cors())
-
-let db = require('./config/database')
-db('mongodb://localhost:27017/4not2020')
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,23 +19,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-let teste = require('./routes/teste')
-app.use('/teste', teste)
-
-const fornecedor = require('./routes/fornecedor')
-app.use('/fornecedor', fornecedor)
-
-const produto = require('./routes/produto')
-app.use('/produto', produto)
-
-const cliente = require('./routes/cliente')
-app.use('/cliente', cliente)
-
-const venda = require('./routes/venda')
-app.use('/venda', venda)
-
-const item_venda = require('./routes/item_venda')
-app.use('/item-venda', item_venda)
 
 module.exports = app;
